e9c0af072924d60e2ec6993f26b826e0d7c9c129
Julia Fedchenko <julia.fdch@yandex.ru>
2022-06-28 01:14:02 +0300

    1st

---
+++ script/index.js
@@ -0,0 +1,29 @@
+const popupElement = document.querySelector('.popup');
+const popupClose = popupElement.querySelector('.popup__close');
+const popupOpen = document.querySelector('.profile__edit');
+
+const openPopup = function() {
+    popupElement.classList.add('popup_opened');
+};
+
+const closePopup = function() {
+    popupElement.classList.remove('popup_opened');
+};
+popupOpen.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+
+let formElement = popupElement.querySelector('.popup__form');
+let nameInput = popupElement.querySelector('.popup__name');
+let jobInput = popupElement.querySelector('.popup__job');
+let Name = document.querySelector('.profile__headline');
+let Job = document.querySelector('.profile__caption');
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    nameInput.value;
+    jobInput.value;
+    Name.textContent = nameInput.value;
+    Job.textContent = jobInput.value;
+    closePopup();
+}
+formElement.addEventListener('submit', formSubmitHandler);
